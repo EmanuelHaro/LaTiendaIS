0f25e828df3fbfb1bbe2b9dbef2f5020624ad780
EmanuelHaro <emanuelharo72@gmail.com>
2024-01-23 12:14:03 -0300

    Add project files.

---
+++ LaTiendaIS.Shared/Models/DBLaTiendaContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LaTiendaIS.Shared.Models
+{
+    public class DBLaTiendaContext : DbContext
+    {
+        public DBLaTiendaContext(DbContextOptions<DBLaTiendaContext> options) : base(options)
+        {
+
+        }
+
+        //public DbSet<Empleado> Empleado { get; set; }
+
+    }
+}
