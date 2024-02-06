541f6541b73c7315ec690d4e1e7e16acc78537fc
EmanuelHaro <emanuelharo72@gmail.com>
2024-02-06 17:26:17 -0300

    Generador de token con api

---
+++ LaTiendaIS.Shared/TokenRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace LaTiendaIS.Shared
+{
+    public class TokenRequest
+    {
+        public string card_number { get; set; }
+        public string card_expiration_month { get; set; }
+        public string card_expiration_year { get; set; }
+        public string security_code { get; set; }
+        public string card_holder_name { get; set; }
+        public CardHolderIdentification card_holder_identification { get; set; }
+    }
+    public class CardHolderIdentification
+    {
+        public string type { get; set; }
+        public string number { get; set; }
+    }
+}
---
+++ LaTiendaIS.Shared/TokenResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaTiendaIS.Shared
+{
+    public class TokenResponse
+    {
+        public string id { get; set; }
+        public string status { get; set; }
+        // Otros campos de la respuesta...
+    }
+}
