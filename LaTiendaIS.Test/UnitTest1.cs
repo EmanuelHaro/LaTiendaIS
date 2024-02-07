cdadc72f026146a746f29093d735c2d30a255382
EmanuelHaro <emanuelharo72@gmail.com>
2024-02-07 16:38:52 -0300

    Agrego Tests Unitarios

---
+++ LaTiendaIS.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+namespace LaTiendaIS.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+
+    }
+}
