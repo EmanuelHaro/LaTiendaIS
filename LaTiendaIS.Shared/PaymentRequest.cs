0276b64db805eb85d20e5fe8cfa73641fc93caa6
EmanuelHaro <emanuelharo72@gmail.com>
2024-02-08 20:36:57 -0300

    Servicio externo pago funcional

--- LaTiendaIS.Shared/PaymentRequest.cs
+++ LaTiendaIS.Shared/PaymentRequest.cs
@@ -8,24 +8,24 @@ namespace LaTiendaIS.Shared
 {
     public class PaymentRequest
     {
-        public string SiteTransactionId { get; set; }
-        public int PaymentMethodId { get; set; }
-        public string Token { get; set; }
-        public string Bin { get; set; }
-        public double Amount { get; set; }
-        public string Currency { get; set; }
-        public int Installments { get; set; }
-        public string Description { get; set; }
-        public string PaymentType { get; set; }
-        public string EstablishmentName { get; set; }
-        public List<SubPayment> SubPayments { get; set; }
+        public string site_transaction_id { get; set; }
+        public int payment_method_id { get; set; }
+        public string token { get; set; }
+        public string bin { get; set; }
+        public double amount { get; set; }
+        public string currency { get; set; }
+        public int installments { get; set; }
+        public string description { get; set; }
+        public string payment_type { get; set; }
+        public string establishment_name { get; set; }
+        public List<SubPayment> sub_payments { get; set; }
     }
 
     public class SubPayment
     {
-        public string SiteId { get; set; }
-        public double Amount { get; set; }
-        public int? Installments { get; set; }
+        public string site_id { get; set; }
+        public double amount { get; set; }
+        public int? installments { get; set; }
     }
 
 }
